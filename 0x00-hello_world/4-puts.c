70b2c654b2ea493829ea4bf544fd8e848bfd3f46
Dr-savant21 <israelnosakhare21@gmail.com>
2022-09-09 04:24:09 -0700

    Task 4 completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Author: Sammykingx
- *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0
+*/
 int main(void)
 {
- char string[70] = "\"Programming is like building a multilingual puzzle\"";
-	
- puts(string);
- /* Tried printing it directly */
-
- puts("\"Programming is like building a multilingual puzzle\"");
- return (0);
+puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
