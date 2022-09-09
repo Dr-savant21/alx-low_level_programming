a5e04b19e74ab9fc00b41297fd5b48f72460269d
Dr-savant21 <israelnosakhare21@gmail.com>
2022-09-09 13:34:14 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Author: Dr-savantcode
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int numberz;
+
+	for (numberz = "0"; numberz < "10"; numberz++)
+		putchar(numberz);
+	putchar("\n");
+	return (0);
+}
