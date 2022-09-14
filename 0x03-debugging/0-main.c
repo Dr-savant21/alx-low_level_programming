025166ee5cb0b57e9147b79d294339164c8bf6d4
Dr-savant21 <israelnosakhare21@gmail.com>
2022-09-14 02:47:02 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function to print if integer is positive or negative
+ * author: Dr-savantcode
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+
+	positive_or_negative(a);
+
+	return (0);
+}
