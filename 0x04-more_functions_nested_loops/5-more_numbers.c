5dfb16bcb608ea9c236d03b48eff853d86b15124
Dr-savant21 <israelnosakhare21@gmail.com>
2022-09-15 18:47:48 -0700

    Task completed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,23 +8,19 @@ void more_numbers(void)
 {
 	int i, j;
 
-	i = 0;
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-		if (j > 9)
-		{
-			_putchar('0' + j / 10);
-		}
-		else
-		{
-			_putchar('0' + j % 10);
-		}
-		j++;
+			if (j > 9)
+			{
+				_putchar('0' + j / 10);
+			}
+			else
+			{
+				_putchar('0' + j % 10);
+			}
 		}
 		_putchar('\n');
-		i++;
 	}
 }
