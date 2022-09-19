8bdd9dcc1ea26f5767e2c0bf404f98bd43c097b0
Dr-savant21 <israelnosakhare21@gmail.com>
2022-09-19 06:00:49 -0700

    task started

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates it to 98
+ * @n: an integer to be returned
+ * author: Dr-savantcode
+ * Return: 0 always success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
